e23b49f2dbe9ec4fbe00e2c0eb36e2e39025992c
Joan <287748018+JOP-DEV@users.noreply.github.com>
2026-05-25 14:13:22 -0500

    feat: set up hilt application and dispatcher dependency module

---
+++ app/src/main/java/com/example/intercommerce_kotlin/core/di/DispatcherModule.kt
@@ -0,0 +1,32 @@
+package com.example.intercommerce_kotlin.core.di
+
+import com.example.intercommerce_kotlin.core.dispatchers.DefaultDispatcher
+import com.example.intercommerce_kotlin.core.dispatchers.IoDispatcher
+import com.example.intercommerce_kotlin.core.dispatchers.MainDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+
+    @Provides
+    @Singleton
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @Singleton
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @Singleton
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
---
+++ app/src/main/java/com/example/intercommerce_kotlin/core/dispatchers/DispatcherQualifiers.kt
@@ -0,0 +1,15 @@
+package com.example.intercommerce_kotlin.core.dispatchers
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
